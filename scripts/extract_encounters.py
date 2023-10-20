83c2b629de02c8eb5611f28914fffe5c8937fd42
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-10-20 17:48:11 -0500

    Python beuatifier crap

--- scripts/extract_encounters.py
+++ scripts/extract_encounters.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 import csv
-from argparse import ArgumentParser,FileType
+from argparse import ArgumentParser, FileType
 
 import pdb
 
@@ -14,9 +14,10 @@ FIRST_ENCOUNTER = "Age at First Patient Engagement"
 FIRST_ENCOUNTER_TYPE = "First Patient Engagement Event"
 LAST_ENCOUNTER = "Age at Last Vital Status"
 
+
 class Encounter:
     def __init__(self, participant_id, age_at, encounter_id=None, event_type=""):
-        self.participant_id =  participant_id
+        self.participant_id = participant_id
         self.age_at = age_at
         self.encounter_id = encounter_id
         self.event_type = event_type
@@ -29,41 +30,48 @@ class Encounter:
     @property
     def key(self):
         return f"P{self.participant_id}-{self.age_at}"
-    
+
     @property
     def id(self):
         if self.encounter_id != None:
             return self.encounter_id
         return self.age_at
-    
+
     @classmethod
     def write_header(cls, writer):
-        writer.writerow([
-            "Participant External ID",
-            "Event ID", 
-            "Event Type",
-            "Age At Event",
-            "First Encounter",
-            "Last Known Encounter"
-        ])
+        writer.writerow(
+            [
+                "Participant External ID",
+                "Event ID",
+                "Event Type",
+                "Age At Event",
+                "First Encounter",
+                "Last Known Encounter",
+            ]
+        )
 
     def write(self, writer):
-        writer.writerow([
-            self.participant_id,
-            self.id,
-            self.event_type,
-            self.age_at,
-            self.first_encounter,
-            self.last_encounter
-        ])
+        writer.writerow(
+            [
+                self.participant_id,
+                self.id,
+                self.event_type,
+                self.age_at,
+                self.first_encounter,
+                self.last_encounter,
+            ]
+        )
 
-if __name__=="__main__":
-    parser = ArgumentParser(description="After looking through the file, this "
-                            "script will attempt to create a data dictionary")
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="After looking through the file, this "
+        "script will attempt to create a data dictionary"
+    )
     parser.add_argument(
         "config",
-        nargs='+',
-        type=FileType('rt'),
+        nargs="+",
+        type=FileType("rt"),
         help="Dataset YAML file with details required to run conversion.",
     )
     args = parser.parse_args()
@@ -78,7 +86,7 @@ if __name__=="__main__":
 
         # Pull Condition events out and build out our discrete event list
         with open(condition_filename, "rt") as condf:
-            conditions = csv.DictReader(condf, delimiter=',', quotechar='"')
+            conditions = csv.DictReader(condf, delimiter=",", quotechar='"')
 
             for line in conditions:
                 # For actual conditions, there is no actual event, so we have
@@ -86,14 +94,16 @@ if __name__=="__main__":
                 if line[AGE_COLNAME] not in ["NA", "Not collected"]:
                     key = "-".join([line[PID_COLNAME], line[AGE_COLNAME]])
                     if key not in encounters:
-                        encounters[key] = Encounter(line[PID_COLNAME],
-                                                    line[AGE_COLNAME],
-                                                    line["Event ID"],
-                                                    line["Event Type"])
-                    
+                        encounters[key] = Encounter(
+                            line[PID_COLNAME],
+                            line[AGE_COLNAME],
+                            line["Event ID"],
+                            line["Event Type"],
+                        )
+
         subject_filename = config["dataset"]["participant"]["filename"]
         with open(subject_filename, "rt") as subf:
-            subjects = csv.DictReader(subf, delimiter=',', quotechar='"')
+            subjects = csv.DictReader(subf, delimiter=",", quotechar='"')
             for line in subjects:
                 pid = line[PID_COLNAME]
                 age1 = line[FIRST_ENCOUNTER]
@@ -109,7 +119,7 @@ if __name__=="__main__":
 
                 # These won't always exist
                 if age2 not in ["NA", "Not collected"]:
-                    #pdb.set_trace()
+                    # pdb.set_trace()
                     k2 = "-".join([pid, age2])
                     if k2 not in encounters:
                         encounters[k2] = Encounter(pid, age2)
@@ -117,14 +127,10 @@ if __name__=="__main__":
 
         encounter_path = filedir / "encounter.csv"
         print(encounter_path)
-        #pdb.set_trace()
+        # pdb.set_trace()
         # For now, we won't worry if the file exists
-        with encounter_path.open('wt') as outf:
-            ewriter = csv.writer(outf, delimiter=',', quotechar='"')
+        with encounter_path.open("wt") as outf:
+            ewriter = csv.writer(outf, delimiter=",", quotechar='"')
             Encounter.write_header(ewriter)
             for enc in encounters:
                 encounters[enc].write(ewriter)
-
-
-
-
